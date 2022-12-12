da93b13e64854456c4227c2881afdd928222ed85
SUMMERRAMSAY <summerdramsay@gmail.com>
2022-12-11 23:56:26 -0500

    Commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const toDoItemsForm = document.querySelector('#toDoItems');
+const listInput = document.querySelector('#toDO');
+const tDList = document.querySelector('ol');
+let toDoList = [];
+
+toDoItemsForm.addEventListener('submit', function)
